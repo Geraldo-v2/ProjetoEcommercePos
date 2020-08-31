93c072d81880f90bb5eb7fd8ef30aa43a849a62a
Geraldo-v2 <geraldo.vjunior@gmail.com>
2020-08-30 22:32:35 -0300

    Adicionado o alterar cadastro do produto
    conectado com mongo, feito o frontend para cadastrar e editar o cadastro

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,6 @@
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from "../constants/productConstants"
-import axios from 'axios';
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL } from "../constants/productConstants"
+import axios from "axios";
+import Axios from "axios";
 const listProducts = () => async (dispatch) => {
     try{
         dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -8,6 +9,30 @@ const listProducts = () => async (dispatch) => {
     }
     catch(error){
         dispatch({ type: PRODUCT_LIST_FAIL,  payload: error.message });
+    }
+}
+const saveProduct = (product) => async (dispatch, getState) =>{
+    try {
+        dispatch({type: PRODUCT_SAVE_REQUEST, payload: product});
+        const {userSignin:{userInfo}} = getState();
+        if(!product._id){
+            const {data} = await Axios.post('/api/products', product, {
+                headers:{
+                    'Authorization': 'Bearer' + userInfo.token
+                }
+            });
+            dispatch({type: PRODUCT_SAVE_SUCCESS, payload: data});
+        }else{
+            const {data} = await Axios.put('/api/products/'+ product._id, product, {
+                headers:{
+                    'Authorization': 'Bearer' + userInfo.token
+                }
+            });
+            dispatch({type: PRODUCT_SAVE_SUCCESS, payload: data});
+        }
+        
+    } catch (error) {
+        dispatch({type: PRODUCT_SAVE_FAIL, payload: error.message});    
     }
 }
 const detailsProduct = (productId) => async (dispatch) =>{
@@ -20,4 +45,4 @@ const detailsProduct = (productId) => async (dispatch) =>{
         dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
     }
 }
-export{ listProducts, detailsProduct }
+export{ listProducts, saveProduct, detailsProduct }
